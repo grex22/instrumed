d4991ea9bc3da62f16f03144bf53d494a5c43cc5
Greg Burkett <greg@ripfish.com>
2016-01-12 13:19:04 -0500

    updating header button

--- themes/instrumed/templates/header.php
+++ themes/instrumed/templates/header.php
@@ -9,7 +9,7 @@
           <a href="<?php echo bloginfo('url'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/instrumed-logo.png" width="195" height="auto"></a>
         </div>
         <div class="col-xs-9 text-right">
-          <a href="#" class="btn btn-primary pull-right">How can we help?</a>
+          <a href="<?php echo get_permalink('contact-us'); ?>" class="btn btn-primary pull-right">How can we help?</a>
           <ul class="nav navbar-nav pull-right">
             <?php wp_nav_menu( array( 'theme_location' => 'primary_navigation', 'walker' => new Bootstrap_Nav_Walker(), 'container' => false,  'items_wrap' => '%3$s') ); ?>
           </ul>
