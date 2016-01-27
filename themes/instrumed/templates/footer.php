755677d76c2df24f27b6d94d28ab69ccc2a2d2f0
Greg Burkett <greg@ripfish.com>
2016-01-27 11:33:42 -0500

    footer menus and styling

--- themes/instrumed/templates/footer.php
+++ themes/instrumed/templates/footer.php
@@ -6,34 +6,13 @@
         <?php dynamic_sidebar('sidebar-footer'); ?>
       </div>
       <div class="col-sm-3 col-xs-4">
-        <h6>Sitemap</h6>
         <?php dynamic_sidebar('sidebar-footer-2'); ?>
-        <ul class="list-unstyled">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Manufacturing</a></li>
-          <li><a href="#">Innovation</a></li>
-          <li><a href="#">Personalization</a></li>
-          <li><a href="#">News</a></li>
-          <li><a href="#">Contact Us</a></li>
-        </ul>
       </div>
       <div class="col-sm-3 col-xs-4">
-        <h6>Manufacturing Services</h6>
         <?php dynamic_sidebar('sidebar-footer-3'); ?>
-        <ul class="list-unstyled">
-          <li><a href="#">Vendor List</a></li>
-          <li><a href="#">Other Services</a></li>
-          <li><a href="#">Surgical Instruments</a></li>
-          <li><a href="#">Surgical Implants</a></li>
-        </ul>
       </div>
       <div class="col-sm-3 col-xs-4">
         <?php dynamic_sidebar('sidebar-footer-4'); ?>
-        <h6>Contact Us</h6>
-        727 N Detroit St<br>
-        Warsaw, IN 46580<br>
-        ph: (574) 269-1982<br>
         
         <div class="btn-group btn-group-social">
           <br>
