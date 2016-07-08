5b209af2b4f72e56861cad4c73da07c1b0e7b28f
Greg Burkett <greg@ripfish.com>
2016-07-08 09:48:48 -0400

    Template edits from july 7th

--- themes/instrumed/templates/header.php
+++ themes/instrumed/templates/header.php
@@ -11,7 +11,15 @@
         </div>
         <div class="col-xs-10 text-right">
           <a href="<?php echo get_permalink(8); ?>" class="pull-right"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/certs.png" width="auto" height="auto"></a>
-          <a href="<?php echo get_permalink('contact-us'); ?>" class="btn btn-primary pull-right visible-lg visible-xl">How can we help?</a>
+          <form class="form-inline pull-right visible-lg visible-xl" action="<?php echo bloginfo('siteurl'); ?>" method="GET">
+            <div class="input-group">
+              <input type="text" class="form-control search-field" name="s" placeholder="Search...">
+              <span class="input-group-btn">
+                <button class="btn btn btn-primary" type="submit"><i class="fa fa-search"></i></button>
+              </span>
+            </div>
+          </form>
+          <!--<a href="<?php echo get_permalink('contact-us'); ?>" class="btn btn-primary pull-right visible-lg visible-xl">How can we help?</a>-->
           <ul class="nav navbar-nav pull-right">
             <?php wp_nav_menu( array( 'theme_location' => 'primary_navigation', 'walker' => new Bootstrap_Nav_Walker(), 'container' => false,  'items_wrap' => '%3$s') ); ?>
           </ul>
